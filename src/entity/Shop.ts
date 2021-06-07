a819d637bf9460e47bd9612d41e9592d72c86e47
Brian Odida <bryodiiidah@gmail.com>
2021-06-07 11:03:53 +0300

    update merchant metadata

--- src/entity/Shop.ts
+++ src/entity/Shop.ts
@@ -2,6 +2,13 @@ import {Column, Entity, JoinTable, ManyToOne, OneToMany, PrimaryGeneratedColumn}
 import {Merchant} from "./Merchant";
 import {Product} from "./Product";
 
+export enum BusinessCategory {
+    BEAUTY_AND_FASHION = "beauty_and_fashion",
+    FOOD_AND_BEVERAGES = "food_and_beverages",
+    ELECTRONICS = "electronics",
+    GENERAL = "general"
+}
+
 @Entity()
 export class Shop {
 
@@ -11,19 +18,55 @@ export class Shop {
     @Column()
     mame: string;
 
+    @Column({
+        nullable: true,
+    })
+    tagLine: string;
+
     @Column()
+    businessPhoneNumber: string;
+
+    @Column({
+        type: "enum",
+        enum: BusinessCategory,
+        default: BusinessCategory.GENERAL
+    })
+    category: string;
+
+    @Column({
+        nullable: true
+    })
+    logoUri: string;
+
+    @Column({
+        default: false
+    })
+    socialMedia: boolean;
+
+    @Column({
+        default: false
+    })
+    artik: boolean;
+
+    @Column({
+        default: false
+    })
+    store: boolean;
+
+    @Column({
+        nullable: true
+    })
     latitude: string;
 
-    @Column()
-    longitude: string;
-
-    @Column()
-    location: string;
+    @Column({
+        nullable: true
+    })
+    longitude: string
 
     @ManyToOne(type => Merchant, merchant => merchant.shop)
     merchant: Merchant;
 
-    @OneToMany(type => Product, product=>product.shop)
+    @OneToMany(type => Product, product => product.shop)
     @JoinTable()
     products: Product[];
 }
